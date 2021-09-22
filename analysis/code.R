6774b6851297280bddf7aecafce26778ce284c40
Martin Kosík <martin.kosiik@gmail.com>
2021-09-22 23:00:39 +0200

    add unmet_demand_scale

--- analysis/code.R
+++ analysis/code.R
@@ -26,13 +26,23 @@ audits_raw_ka <- read_csv("C:/Users/marti/OneDrive/Plocha/research_projects/scra
 
 
 audits_by_panch <- audits_raw_ka %>% 
-  mutate(unmet_demand_dummy = unmet_demand %in% c('Yes, Huge Demand', 'Yes, Some Demand')) %>% 
+  mutate(unmet_demand_dummy = unmet_demand %in% c('Yes, Huge Demand', 'Yes, Some Demand'),
+         unmet_demand_scale = case_when(unmet_demand == 'Yes, Huge Demand' ~ 2,
+                                        unmet_demand == 'Yes, Some Demand' ~ 1,
+                                        unmet_demand %in% c('No', 'No, people get work when they want it') ~ 1),
+         diff_in_total_exp = total_exp - total_given,
+         diff_in_total_exp_pct = abs(diff_in_total_exp)/ total_given,
+         diff_in_total_exp_log = log(abs(diff_in_total_exp) + 1)) %>% 
   group_by(panchayat_id) %>% 
   summarize(n_audits = n(),
             mean_unmet_demand = mean(unmet_demand_dummy, na.rm = T),
+            mean_unmet_demand_scale = mean(unmet_demand_scale, na.rm = T),
             mean_issues_rep = mean(total_issues_reported, na.rm = T),
             mean_fm_amount = mean(fm_amount, na.rm = T),
-            mean_fm_rep =  mean(fm_reported, na.rm = T)
+            mean_fm_rep =  mean(fm_reported, na.rm = T),
+            diff_in_total_exp_pct =  mean(diff_in_total_exp_pct, na.rm = T),
+            diff_in_total_exp_log =  mean(diff_in_total_exp_log, na.rm = T)
+            
   )
   
 
